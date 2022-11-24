583d7996651b68ac3f6bf2f54a49be78f615ce10
Salmalisa <salmalisa7342@gmail.com>
2022-11-24 20:42:36 +0600

    all accounts store api created

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 
 //middleware
 app.use(cors())
@@ -12,6 +14,29 @@ app.get('/', (req, res) => {
   res.send("budget cars server is ready to use")
 })
 
+//mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.t3mwvsa.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri)
+async function run() {
+  try {
+    const allAccountsCollection = client.db('budgetCarsDB').collection('allAccounts')
+
+    app.post('/allAccounts', async (req, res) => {
+      const newlyCreatedAccount = req.body 
+      const result = await allAccountsCollection.insertOne(newlyCreatedAccount)
+      res.send(result)
+    })
+  }
+  finally {
+    
+  }
+}
+
+
+client.connect(err => {
+ console.log(err)
+});
 
 
 
