82058aba5078beecfbbe76e8e918d481fb6fc5f4
Salmalisa <salmalisa7342@gmail.com>
2022-11-28 16:37:46 +0600

    reported item delete api added

--- index.js
+++ index.js
@@ -350,6 +350,14 @@ async function run() {
       const result = await reportedItemsCollection.find(query).toArray();
       res.send(result);
     });
+//==============================
+    //reported item delete 
+    app.delete("/reportedItems/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reportedItemsCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
